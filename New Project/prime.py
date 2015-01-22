d2d235d58f63b5df45065bfb50e3c80c0153756b
Thejus Krishna <thejuskrishna@gmail.com>
2015-01-22 23:43:52 +0530

    Created by Coding Ground Client

---
+++ New Project/prime.py
@@ -0,0 +1,12 @@
+
+def inputNumber(number):
+    i = 2;
+    for i in xrange(2, number/2):
+        if number % i == 0 and number != i:
+            number = number / i
+            print(i)
+        if number == i:
+            return number
+    return -1
+print inputNumber(600851475143)
+        
